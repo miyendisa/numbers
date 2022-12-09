26be1412e9aa3c025f64abfc1ac3e60b5140cbdb
Aslıhan <aslihan_hasar@hotmail.com>
2022-12-09 20:34:29 +0300

    Add program codes via upload

--- src/MainThree.java
+++ src/MainThree.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 
-public class MainOne {
+public class MainThree {
     public static void main(String[] args) {
-        int number, i;
+        int number, i,average;
+        int sum = 0, counter = 0;
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
         number = scanner.nextInt();
-        System.out.println("Even Numbers: ");
 
         for (i = 1; i < number; i++) {
-            if (i % 2 == 0) {
+            if ((i % 3 == 0) && (i % 4 == 0)) {
+                sum += i;
+                counter++;
                 System.out.println(i);
             }
         }
+        average = sum / counter;
+        System.out.println("Average: " + average);
     }
 }
--- src/MainTwo.java
+++ src/MainTwo.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 
-public class MainOne {
+public class MainTwo {
     public static void main(String[] args) {
-        int number, i;
+        int number, i = 1;
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
         number = scanner.nextInt();
         System.out.println("Even Numbers: ");
 
-        for (i = 1; i < number; i++) {
+        while (i < number) {
             if (i % 2 == 0) {
                 System.out.println(i);
             }
+            i++;
         }
     }
 }
